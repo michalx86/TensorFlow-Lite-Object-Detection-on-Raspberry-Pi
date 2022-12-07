84ea11704827c124366ae6c337b77c5d59575ff9
Michal Piotrowski <michal.piotrowski@redembedded.com>
2022-12-07 17:44:58 -0500

    Main window resized to 1000x1000

--- TFLite_detection_webcam.py
+++ TFLite_detection_webcam.py
@@ -230,8 +230,9 @@ while True:
     cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
     cv2.putText(frame,'Inference: {:_>3.0f} ms'.format((end_time - start_time) * 1000),(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
 
+    frame_large = cv2.resize(frame, (1000,1000))
     # All the results have been drawn on the frame, so it's time to display it.
-    cv2.imshow('Object detector', frame)
+    cv2.imshow('Object detector', frame_large)
 
     # Calculate framerate
     t2 = cv2.getTickCount()
