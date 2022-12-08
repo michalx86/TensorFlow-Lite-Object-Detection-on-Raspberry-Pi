b934082cbc9f8cf669c7331671946fb3fea8d099
Michal Piotrowski <michal.piotrowski@redembedded.com>
2022-12-08 16:42:32 +0100

    efficientdet0-lite model added

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-
-#python3 TFLite_detection_webcam.py --modeldir=pets_model/ --graph=ssd_mobilenet_v2_catsdogs_quant.tflite
-#python3 TFLite_detection_webcam.py --modeldir=gesture_model/ --graph=ssd_mobilenet_v2_gestures.tflite
+# python3 TFLite_detection_webcam.py --modeldir=gesture_model/ --graph=efficientdet0-lite-gestures.tflite --resolution=640x480
 python3 TFLite_detection_webcam.py --modeldir=gesture_model/ --graph=ssd_mobilenet_v2_gestures.tflite --resolution=640x480
 
